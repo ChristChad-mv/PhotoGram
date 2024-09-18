ace526d21ecb9792e612f342058b062d39841815
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-09-18 20:11:56 +0200

    Creating project. Configuration for Firebase and tailwindcss for all the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p className=" text-2xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
