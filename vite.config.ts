b2a127b06a0824ea7fcb40f983a0fbf794617355
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-09-20 19:23:33 +0200

    Some change in the core part of the project such as adding dependecies, installation of shadcn UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
-    
-  }
+  }, 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
