68c43adc4944a3159db375e0ad89d97fbe100852
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-09-20 19:10:49 +0200

    Reconfigure firebaseConfig file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDYpbZZn-ZMdQQCK_wRIF77Zq0XNyGJJio",
+  authDomain: "photogram-d4b18.firebaseapp.com",
+  projectId: "photogram-d4b18",
+  storageBucket: "photogram-d4b18.appspot.com",
+  messagingSenderId: "893508735126",
+  appId: "1:893508735126:web:eaad28ef37040fee6cb2b7",
 };
 
 // Initialize Firebase
