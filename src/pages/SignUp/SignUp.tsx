0d51cb08c9fc941ac06603bf37cad08c29f9085c
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-09-24 23:42:41 +0200

    FInishing the SignUp component with clean use case of interface, UI component and some style

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -3,12 +3,8 @@ import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
 import { useUser } from '@/context/userAuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import { IUserSignIn } from '../../types/types';
 
-interface IUserInfo {
-  name: string, 
-  email: string, 
-  password: string,
-}
 
 const initialValue = {
   name: '', 
@@ -16,13 +12,12 @@ const initialValue = {
   password: ''
 }
 
-
 interface ISignUpProps {
 }
 
 const SignUp: React.FunctionComponent<ISignUpProps> = (props) => {
-  const [userInfo, setUserInfo] = React.useState<IUserInfo>(initialValue)
-  const { googleSignIn } = useUser();
+  const [userInfo, setUserInfo] = React.useState<IUserSignIn>(initialValue)
+  const { googleSignIn, logIn } = useUser();
   const navigate = useNavigate();
 
   const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -58,31 +53,59 @@ const SignUp: React.FunctionComponent<ISignUpProps> = (props) => {
     }
   }
 
+  const handleSubmitForm = async (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    try {
+      const newUser = await logIn(userInfo.email, userInfo.password);
+      console.log(newUser.user);
+      
+      navigate('/login');
+
+      /**
+       * TODOOO
+       * Just after successfull creation of user, you have to store the user data in the database. 
+       * So we have to configure the firestore to store the future data of other user.
+       * 
+       */
+      
+    } catch (err) {
+      console.log('Erreur : ', err)
+    }
+  }
+
   
   return (
     <div className='flex items-center justify-center h-screen bg-primary'>
-      <div className='w-[25%] border rounded-md text-center p-4 bg-background'>
-        <form>
-          <h1 className='my-4 font-bold'>PHOTOGRAM</h1>
+      <div className='w-[25%] border rounded-md py-4 bg-background px-6'>
+        <form onSubmit={handleSubmitForm}>
+        
+          <h1 className='my-4 text-4xl font-bold text-center text-transparent bg-custom-gradient bg-clip-text'>PHOTOGRAM </h1>
           
-          <p className='my-4'>Create your account</p>
+          <p className='my-4 text-center'>Creating a account</p>
           
           <div className='flex justify-center my-4 space-x-4'>
             <Button label='Google' onClick={signUpWithGoogle} />
             <Button label='Facebook' onClick={signUpWithGoogle} />
           </div>
 
-          <div className='w-full '><span className='z-10'>OR CONNECT WITH EMAIL AND PASSWORD</span></div>
+          <div className='flex items-center justify-center'><span className='z-10'>OR</span></div>
 
           {/* Input elements for collect data from user  */}
           <div className='flex-col my-4 space-y-2'>
-            <Input type='text' onChange={handleNameInput} placeholder='Enter your name' value={userInfo.name} />
-            <Input type='email' onChange={handleEmailInput} placeholder='Enter your email account' value={userInfo.email} />
-            <Input type='password' onChange={handlePasswordInput} placeholder='Enter your your password' value={userInfo.password} />
+            <Input type='text' onChange={handleNameInput} placeholder='Enter your name' value={userInfo.name} label='Name' />
+            <Input type='email' onChange={handleEmailInput} placeholder='Enter your email account' value={userInfo.email} label='Email' />
+            <Input type='password' onChange={handlePasswordInput} placeholder='Enter your your password' value={userInfo.password} label='Password' />
+          </div>
+
+          <div className='flex items-center justify-center'>
+            <button type='submit' className='px-6 py-2 text-white ease-in rounded-lg bg-primary hover:bg-slate-700'>
+              Create an account
+            </button>
           </div>
 
           {/* Footer card sign up */}
-          <div className='my-4'>
+          <div className='my-4 text-center'>
             <p>You already have a account ? <Link className='font-semibold' to='/login'>Log In</Link> </p>
           </div>
         </form>
