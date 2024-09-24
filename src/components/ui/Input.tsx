291ccf5048ccd17f6f531fca35efbb88857a20b1
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-09-24 23:31:38 +0200

    Modify the Input UI file by bringing cool stuff on it. And adding also the label props for more description of each inputs

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,19 +5,22 @@ interface IInputProps {
   placeholder: string, 
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
   value: string, 
+  label: string,
 }
 
-const Input: React.FunctionComponent<IInputProps> = ({ type, placeholder, onChange, value }) => {
+const Input: React.FunctionComponent<IInputProps> = ({ type, placeholder, onChange, value, label }) => {
   return (
-      <input
-        type={type}
-        placeholder={placeholder}
-        onChange={onChange}
-        value={value}
-        className='w-full p-2 my-2 border rounded-md outline-none'
-      >
-
-      </input>
+      <div className='mt-2'>
+          <h1 className='text-[17px]'> {label} </h1>
+          <input
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          className='w-full p-2 my-2 border rounded-md outline-none border-primary'
+        >
+        </input>
+      </div>
   );
 };
 
